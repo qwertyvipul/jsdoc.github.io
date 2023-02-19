167898bae8fa9b172430ccce978317713dfe0b69
Vipul Sharma <vipuls526@gmail.com>
2023-02-19 15:01:54 +0530

    Added initial content (partial)

---
+++ docusaurus.config.js
@@ -0,0 +1,67 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: 'JSDoc Docs',
+    tagline: 'An API documentation generator for JavaScript.',
+
+    // Set the production url of your site here
+    url: 'https://jsdoc.app',
+    baseUrl: '/',
+    organizationName: 'JSDoc',
+    projectName: 'jsdoc.github.io',
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    routeBasePath: '/',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: '@jsdoc',
+                items: [
+                    {
+                        href: 'https://github.com/jsdoc/jsdoc',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
+};
+
+module.exports = config;
